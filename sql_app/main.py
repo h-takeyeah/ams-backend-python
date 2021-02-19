1b35ef674319a19e8d6396d267871b5a47e7cd37
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-02-20 00:06:36 +0900

    Handle h11._util.LocalProtocolError: Too much data for declared Content-Length

--- sql_app/main.py
+++ sql_app/main.py
@@ -5,6 +5,8 @@ from fastapi import FastAPI
 from fastapi.exceptions import HTTPException
 from fastapi.param_functions import Depends
 from sqlalchemy.orm import Session
+from starlette import status
+from starlette.responses import Response
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -13,8 +15,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
+
 def get_db():
+    """Dependency"""
     db = SessionLocal()
     try:
         yield db
@@ -22,7 +25,7 @@ def get_db():
         db.close()
 
 
-@app.post('/accesslogs', response_model=schemas.AccessLog, status_code=201)
+@app.post('/accesslogs', response_model=schemas.AccessLog, status_code=status.HTTP_201_CREATED)
 def create_accesslog(log: schemas.AccessLog, db: Session = Depends(get_db)):
     return crud.create_accesslog(db, log=log)
 
@@ -50,34 +53,35 @@ def read_inroomuser(parsed_id: int, db: Session = Depends(get_db)):
     user = crud.get_inroomuser(db, user_id=parsed_id)
     if user is None:
         raise HTTPException(
-            status_code=404, detail=f'User({parsed_id}) is not in the room')
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'User({parsed_id}) is not in the room')
     return user
 
 
-@app.post('/room', response_model=schemas.InRoomUser, status_code=201)
+@app.post('/room', response_model=schemas.InRoomUser, status_code=status.HTTP_201_CREATED)
 def create_inroomuser(user: schemas.InRoomUserBase, db: Session = Depends(get_db)):
     try:
         db_user = crud.create_inroomuser(db, user=user)
     except:
         raise HTTPException(
-            status_code=409, detail=f'Conflict! User({user.id}) is already in the room')
+            status_code=status.HTTP_409_CONFLICT, detail=f'Conflict! User({user.id}) is already in the room')
     return db_user
 
 
-@app.delete('/room/{parsed_id}', response_model=None, status_code=204)
+@app.delete('/room/{parsed_id}', response_model=None)
 def delete_and_move_inroomuser(parsed_id: int, db: Session = Depends(get_db)):
     """
-    1. copy(if exists)
-    2. delete original
-    3. paste
+    1. copy
+    2. paste
+    3. delete
     """
-    user = crud.get_inroomuser(db, user_id=parsed_id)  # 1. copy(if exists)
+    user = crud.get_inroomuser(db, user_id=parsed_id)  # 1. copy
     if user is None:
         raise HTTPException(
             status_code=404, detail=f'Cannot delete non-existent User({parsed_id})')
-    crud.delete_inroomuser(db, user=user)  # 2. delele
 
     log = schemas.AccessLog(
         student_id=user.id, entered_at=user.entered_at, exited_at=datetime.now())
-    crud.create_accesslog(db, log)  # 3. paste
-    return
+
+    crud.create_accesslog(db, log=log)  # 2. paste
+    crud.delete_inroomuser(db, user=user)  # 3. delele
+    return Response(status_code=status.HTTP_204_NO_CONTENT)  # content-length: 0
