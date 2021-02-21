5e5617fb1ee34cca6b6495aee0dc79dde1706129
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2021-02-21 15:48:49 +0900

    Integer signed -> unsigned

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column
 from sqlalchemy.sql.expression import text
-from sqlalchemy.sql.sqltypes import DateTime, Integer
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.dialects.mysql import INTEGER as MySQLInteger
 
 from .database import Base
 
@@ -8,7 +9,7 @@ from .database import Base
 class AccessLog(Base):
     __tablename__ = 'accesslogs'
 
-    student_id = Column(Integer, primary_key=True)
+    student_id = Column(MySQLInteger(unsigned=True), primary_key=True)
     entered_at = Column(DateTime, primary_key=True, index=True)
     exited_at = Column(DateTime, default=None, index=True)
 
@@ -16,5 +17,5 @@ class AccessLog(Base):
 class InRoomUser(Base):
     __tablename__ = 'inroomusers'
 
-    id = Column(Integer, primary_key=True, autoincrement=False)
+    id = Column(MySQLInteger(unsigned=True), primary_key=True, autoincrement=False)
     entered_at = Column(DateTime, server_default=text('CURRENT_TIMESTAMP()'))
